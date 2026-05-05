f2dbbcf85b1855d7a4159d5d32f17ab958a0f79e
opkrish031 <pratyakshjangid2@gmail.com>
2026-05-05 15:34:21 +0530

    perf: enable Sanity CDN for production

--- lib/sanity.ts
+++ lib/sanity.ts
@@ -5,7 +5,7 @@ export const client = createClient({
   projectId: process.env.NEXT_PUBLIC_SANITY_PROJECT_ID || 'dummy',
   dataset: process.env.NEXT_PUBLIC_SANITY_DATASET || 'production',
   apiVersion: '2024-01-01',
-  useCdn: false,
+  useCdn: process.env.NODE_ENV === 'production',
   token: process.env.SANITY_API_TOKEN,
 })
 
